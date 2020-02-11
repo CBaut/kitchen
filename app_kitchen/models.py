96c7a89211decb7e57639217a8be001670c4f444
faisalsrs <faisalsrs@gmail.com>
2020-02-11 15:54:00 -0800

    added Models

--- app_kitchen/models.py
+++ app_kitchen/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib import messages
+import re
 
-# Create your models here.
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def validator(self, data):
+        errors = {}
+        if len(data['fname'])<2:
+            errors['fname'] = "First Name needs to be longer"
+        if len(data['lname'])<2:
+            errors['lname'] = "Last Name needs to be longer"
+        if not EMAIL_REGEX.match(data['email']):
+            errors['email'] = "Email is invalid"
+        if len(data['password'])<8:
+            errors['password']= "Password must be 8 characters or longer"
+        if data['password']!=data['cpassword']:
+            errors['password'] = "Passwords do not match"
+        return errors
+
+class ItemManager(models.Manager):
+    def validator(self, data):
+        errors={}
+        if data['image_link']=="":
+            errors['profile']="Must have image url"
+            return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+class ItemsToOrder(models.Model):
+    items = models.CharField(max_length=60) - one to many
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(auto_now=True)
+
+class Item(models.Model):
+    quantity = models.IntegerField()
+    is_requested = models.IntegerField()
+    image_link = models.TextField()
+    is_low_inventory = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ItemManager()
+    last_updated_by = models.OneToOneField(to=User)
+
+class OrderHistory(models.Model):
+    quantity_ordered = models.IntegerField()
+    date_replenished = models.DateTimeField(auto_now=True)
